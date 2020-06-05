69a924a92ad24c401381c02b586c5a6f4fec7612
Priyanka Agrawal <65527151+Priyanka-tech798@users.noreply.github.com>
2020-06-05 19:37:47 +0530

    Update Multiply_all_the_elements_in_list.py

--- Multiply_all_the_elements_in_list.py
+++ Multiply_all_the_elements_in_list.py
@@ -1,12 +1,12 @@
 import numpy
 lst = [5,2,3]
 result = numpy.prod(lst)
-print(result)
+print(result)   #30
 
 
 
 from functools import reduce
 lst = [5,2,3]
 result = reduce(lambda x,y:x*y,lst)
-print(result)
+print(result)    #30
 
